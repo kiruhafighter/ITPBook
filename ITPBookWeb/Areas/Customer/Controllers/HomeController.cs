50ee2bc410aebadb3029bc15b8f9327aef6febd0
Kyrylo Salomatin <kirill.salomatin@itperspectives.ro>
2022-11-28 13:49:47 +0200

    Homepage controller and view added

--- ITPBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ ITPBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿
+using ITPBook.DataAccess.Repository.IRepository;
 using ITPBook.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -9,17 +10,28 @@ namespace ITPBookWeb.Areas.Customer.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    private readonly IUnitOfWork _unitOfWork;
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
+        return View(productList);
     }
+    public IActionResult Details(int id)
+    {
+        ShoppingCart cartObj = new()
+        {
+            Count = 1,
+            Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,CoverType")
+        };
 
+        return View(cartObj);
+    }
     public IActionResult Privacy()
     {
         return View();
